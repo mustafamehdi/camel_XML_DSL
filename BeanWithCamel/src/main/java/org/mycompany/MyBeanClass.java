46bd7df29a17c94138f9695efcf87ef601e8f223
Mustafa <mustafa.5657@gmail.com>
2021-10-15 11:27:44 -0400

    Added below projects: 1) bean with camel 2) seda with camel

---
+++ BeanWithCamel/src/main/java/org/mycompany/MyBeanClass.java
@@ -0,0 +1,14 @@
+package org.mycompany;
+
+public class MyBeanClass {
+
+	public void test_bean() {
+		
+		System.out.println("This is my bean that will be used in camel route");
+	}
+	
+	public void secondMethod() {
+		
+		System.out.println("This is second method in bean");
+	}
+}
